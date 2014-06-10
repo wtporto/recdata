cf6ef8437f6233527c16517d3f748290e7b5a0a5
wemerson.thayne <wemerson.thayne@gmail.com>
2014-06-10 17:45:12 +0000

    Versão atualizada

---
+++ ReCDATA/src/br/edu/ifpb/Tela2.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tela2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            // TODO Auto-generated method stub
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.telalogin);
+    }
+}
---
+++ ReCDATA/src/br/edu/ifpb/TelaAbertura.java
@@ -0,0 +1,38 @@
+package br.edu.ifpb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TelaAbertura extends Activity implements Runnable{
+
+
+	private int duracao_da_tela = 3000;
+
+
+	@Override
+		protected void onCreate(Bundle savedInstanceState) {
+ 
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_abertura);
+     
+        new Handler().postDelayed(this, duracao_da_tela);
+	
+	}
+
+		@Override
+		public void run() {
+		
+		        Intent minha_acao = new Intent(this, Tela2.class);
+		        
+		        startActivity(minha_acao);
+		        
+		        finish();
+		        
+		}
+}
+
+

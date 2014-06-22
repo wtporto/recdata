2556d84ec7fef656db4f29b465f72fa3a781b113
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-06-22 00:57:14 +0000

    Versão do Projeto para Android com algumas funcionalidades pronta, mas tudo muito básico, nesta versão já contém a Splash, Tela de Login, Lista de Funcionalidades e suas respctivas telas,

---
+++ trunk/ReCDATA/src/br/edu/ifpb/TelaLogin.java
@@ -0,0 +1,37 @@
+package br.edu.ifpb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TelaLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+   
+       final Button logarbutton = (Button) findViewById(R.id.LoginButton);
+        logarbutton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				EditText login =(EditText) findViewById(R.id.LoginApp);
+				String nomeUsuario = login.getText().toString();
+				System.out.println("Nome Login:"+nomeUsuario);
+				
+			    Toast.makeText(getApplicationContext(),"Bem Vindo "+nomeUsuario, Toast.LENGTH_LONG).show(); 				 
+			    Intent chamarLista = new Intent(TelaLogin.this, TelaListaFuncionalidades.class);
+			    startActivity(chamarLista);
+			}
+		});
+        
+	}
+   
+}
---
+++ trunk/ReCDATA/src/br/edu/ifpb/TelaReservar.java
@@ -0,0 +1,30 @@
+package br.edu.ifpb;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class TelaReservar extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.listaactivity_tela_reserva);
+		 final Button buscabutton = (Button) findViewById(R.id.BuscaButton);
+	     buscabutton.setOnClickListener( new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+				    Toast.makeText(getApplicationContext(),"Buscando Informações...", Toast.LENGTH_LONG).show(); 				 
+				     
+				}
+	     });
+	}
+}

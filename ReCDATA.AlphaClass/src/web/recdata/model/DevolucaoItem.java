e7f1856d35639bc76e92f121a1fae14564636a7d
marciomcbs <marciomcbs@gmail.com>
2014-10-08 14:18:09 +0000

    - Inicio do desenvolvimento do Reserva. - Criação das classes Devolução.

---
+++ ReCDATA.AlphaClass/src/web/recdata/model/DevolucaoItem.java
@@ -0,0 +1,45 @@
+package web.recdata.model;
+
+import java.util.Date;
+
+public class DevolucaoItem implements Entidade {
+
+	private int idDevolucao;
+	private int usuarioIdDevolucao;
+	private int itemIdDevolucao;
+	private Date horaDataDevolucao;
+	public int getIdDevolucao() {
+		return idDevolucao;
+	}
+	public void setIdDevolucao(int idDevolucao) {
+		this.idDevolucao = idDevolucao;
+	}
+	public int getUsuarioIdDevolucao() {
+		return usuarioIdDevolucao;
+	}
+	public void setUsuarioIdDevolucao(int usuarioIdDevolucao) {
+		this.usuarioIdDevolucao = usuarioIdDevolucao;
+	}
+	public int getItemIdDevolucao() {
+		return itemIdDevolucao;
+	}
+	public void setItemIdDevolucao(int itemIdDevolucao) {
+		this.itemIdDevolucao = itemIdDevolucao;
+	}
+	public Date getHoraDataDevolucao() {
+		return horaDataDevolucao;
+	}
+	public void setHoraDataDevolucao(Date horaDataDevolucao) {
+		this.horaDataDevolucao = horaDataDevolucao;
+	}
+	@Override
+	public String toString() {
+		return "DevolucaoItem [idDevolucao=" + idDevolucao
+				+ ", usuarioIdDevolucao=" + usuarioIdDevolucao
+				+ ", itemIdDevolucao=" + itemIdDevolucao
+				+ ", horaDataDevolucao=" + horaDataDevolucao + "]";
+	}
+
+	
+
+}

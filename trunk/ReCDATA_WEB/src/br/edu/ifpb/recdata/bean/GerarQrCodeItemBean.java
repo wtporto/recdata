c39f4ac62ef35a3bea90495c0124ad2a37617daa
rhavy.maia <rhavy.maia@gmail.com>
2014-10-23 20:46:46 +0000

    - Template JSF; - Dependências IReport.

---
+++ trunk/ReCDATA_WEB/src/br/edu/ifpb/recdata/bean/GerarQrCodeItemBean.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.recdata.bean;
+
+public class GerarQrCodeItemBean {
+
+	public String gererPdfQrCodeItem() {
+		return null;		
+	}
+	
+	public String gererPdfQrCodeItens() {
+		return null;		
+	}
+}

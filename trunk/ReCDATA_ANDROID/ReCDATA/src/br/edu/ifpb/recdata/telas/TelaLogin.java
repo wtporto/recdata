631f15456f8c7b2fd1e0603c09091e89425404c3
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-10-24 17:15:35 +0000

    -alterações na validação dos campos de login e senha

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaLogin.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaLogin.java
@@ -12,65 +12,71 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import br.edu.ifpb.R;
 import br.edu.ifpb.recdata.servicos.UsuarioLoginAsyncTask;
+import br.edu.ifpb.recdata.util.*;
 
-public class TelaLogin extends Activity  implements OnClickListener {
+public class TelaLogin extends Activity implements OnClickListener {
 
 	EditText login;
 	EditText senha;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_login);
-   
-		login =(EditText) findViewById(R.id.LoginApp);
-		senha= (EditText) findViewById(R.id.SenhaApp);
- 
-        
-       final Button logarbutton = (Button) findViewById(R.id.LoginButton);
-       logarbutton.setOnClickListener( this);
-        
-        final TextView txtCadastrar =(TextView) findViewById(R.id.textview_cadastrar);
-        
-        txtCadastrar.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-            	 Intent chamarCadastro = new Intent(TelaLogin.this, TelaCadastraUsuario.class);
- 			    startActivity(chamarCadastro);
- 			    finish();
-            }
-       });
-         
-	}
-    
-    private JSONObject logarUsuario() {
-        JSONObject jsonObject = new JSONObject();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tela_login);
 
-        try {
-                jsonObject.put("loginUsuario", login.getText());
-                jsonObject.put("senhaUsuario", senha.getText());
-                
-        } catch (JSONException e) {
-                Log.e("ReCDATA", e.getMessage());
-                // Toast para o usuário com erro mais suave.
-        }
-        return jsonObject;
-}
+		login = (EditText) findViewById(R.id.LoginApp);
+		senha = (EditText) findViewById(R.id.SenhaApp);
 
-  
+		final Button logarbutton = (Button) findViewById(R.id.LoginButton);
+		logarbutton.setOnClickListener(this);
+
+		final TextView txtCadastrar = (TextView) findViewById(R.id.textview_cadastrar);
+
+		txtCadastrar.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View arg0) {
-
-				
-				JSONObject jsonObject = logarUsuario();
-				UsuarioLoginAsyncTask usuarioLogar = new UsuarioLoginAsyncTask(this);
-                usuarioLogar.execute(jsonObject);
-                
+			public void onClick(View v) {
+				Intent chamarCadastro = new Intent(TelaLogin.this,
+						TelaCadastraUsuario.class);
+				startActivity(chamarCadastro);
+				finish();
 			}
-    }
+		});
 
+	}
 
+	private JSONObject logarUsuario() {
+		JSONObject jsonObject = new JSONObject();
+
+		try {
+			jsonObject.put("loginUsuario", login.getText());
+			jsonObject.put("senhaUsuario", senha.getText());
+
+		} catch (JSONException e) {
+			Log.e("ReCDATA", e.getMessage());
+			// Toast para o usuário com erro mais suave.
+		}
+		return jsonObject;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+
+		if ((Metodos.ValidarCampoEditText(login) == true)
+				&& (Metodos.ValidarCampoEditText(senha) == true)) {
+			
+			
+			JSONObject jsonObject = logarUsuario();
+			UsuarioLoginAsyncTask usuarioLogar = new UsuarioLoginAsyncTask(this);
+			usuarioLogar.execute(jsonObject);
+
+		} else if ((Metodos.ValidarCampoEditText(login) == false)
+				&& (Metodos.ValidarCampoEditText(senha) == false)) {
+
+			
+
+		}
+	}
+}

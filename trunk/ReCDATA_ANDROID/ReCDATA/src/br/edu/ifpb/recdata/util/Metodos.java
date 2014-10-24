a71630dc89515fcf1ed4ee799809116c2a738cd2
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-10-24 17:35:31 +0000

    -método de validação do ediText

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Metodos.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Metodos.java
@@ -1,5 +1,25 @@
 package br.edu.ifpb.recdata.util;
 
+import android.widget.EditText;
+
 public class Metodos {
 
+	private static String ErroPreenche= "CAMPO NÃO PREENCHIDO";
+	
+	public static boolean ValidarCampoEditText(EditText campo){
+		
+		 boolean verificarConteudo = false;
+		
+		 if ((campo.getText().toString().trim().equals(""))||(campo.getText().toString().equals(null))) {
+			campo.setError(ErroPreenche.toString());
+			campo.setFocusable(true);
+			campo.setHintTextColor(12);
+			verificarConteudo = false;
+		}else{
+			verificarConteudo=true;
+		    campo.setFocusable(false);
+		}
+		return verificarConteudo;
+		
+	}
 }

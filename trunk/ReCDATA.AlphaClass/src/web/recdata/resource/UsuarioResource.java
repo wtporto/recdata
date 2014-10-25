9650378d018b1f9f039d14510e811bdb25eafcf9
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-10-25 22:15:41 +0000

    -motificação para o serviço de login do usuário

--- trunk/ReCDATA.AlphaClass/src/web/recdata/resource/UsuarioResource.java
+++ trunk/ReCDATA.AlphaClass/src/web/recdata/resource/UsuarioResource.java
@@ -1,4 +1,4 @@
-package web.recdata.resource;
+ package web.recdata.resource;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,8 +10,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.ResponseProcessingException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import web.recdata.controller.UsuarioController;
+import br.edu.ifpb.recdata.entidades.Erro;
 import br.edu.ifpb.recdata.entidades.Usuario;
 
 @Path("/usuario")
@@ -28,10 +32,32 @@ public class UsuarioResource {
 	@Path("/verificar")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public boolean verificaLogin(Usuario user) {
-		return new UsuarioController().verificaLogin(user);
-	}
+	public Response  verificaLogin(Usuario user) {
+		  
+		ResponseBuilder builder = Response.status(Response.Status.BAD_REQUEST);
+		Usuario usuarioConsulta= new Usuario();
+		  
+		try {
 
+			
+			usuarioConsulta= new UsuarioController().verificaLogin(user);       
+            
+            builder.status(Response.Status.OK);
+            builder.entity(usuarioConsulta);
+
+	    } catch (Exception e) {
+	            Erro erro = new Erro();
+	            erro.setCodigo(1);
+	            erro.setMensagem("Não Foi Possivel encontra Usuario");
+	
+	            builder.status(Response.Status.INTERNAL_SERVER_ERROR).entity(
+	                            erro);
+	    }
+	
+	return builder.build();
+	
+	}
+	
 	@POST
 	@Path("/criar")
 	@Consumes("application/json")

3f4aac31c854fb186c160680abc9077a8c58d976
rhavy.maia <rhavy.maia@gmail.com>
2014-10-26 12:55:35 +0000

    - Buscar Item.

--- trunk/ReCDATA_WEB/src/br/edu/ifpb/recdata/bean/ListarItensBean.java
+++ trunk/ReCDATA_WEB/src/br/edu/ifpb/recdata/bean/ListarItensBean.java
@@ -1,21 +1,42 @@
 package br.edu.ifpb.recdata.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+import br.edu.ifpb.recdata.entidades.Item;
+import br.edu.ifpb.recdata.service.ProviderServiceFactory;
+import br.edu.ifpb.recdata.service.ReCDATAService;
 
 @ManagedBean(name="listarItensBean")
-@RequestScoped
-public class ListarItensBean {
+@SessionScoped
+public class ListarItensBean extends Item {	
 	
-	private String texto;
+	public String mensagem;
+	
+	ReCDATAService service = ProviderServiceFactory
+			.createServiceClient(ReCDATAService.class);
+	
+	public String listarItens() {
+		String navegacao = null;	
 
-	public String getTexto() {
-		return "Testando";
+		Item item = new Item();
+		item.setDescricaoItem(getDescricaoItem());
+		List<Item> itens = this.service.consultarItens(item);
+		
+		mensagem = itens.toString();
+		
+		System.out.println(mensagem);
+		
+		return navegacao;
 	}
 
-	public void setTexto(String texto) {
-		this.texto = texto;
+	public String getMensagem() {
+		return mensagem;
 	}
-	
-	
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 }

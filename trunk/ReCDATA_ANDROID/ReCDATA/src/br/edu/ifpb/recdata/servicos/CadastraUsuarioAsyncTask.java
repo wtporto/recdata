1e294f2b7dfd5bdf56b4fc2f082f02f2e307ffc6
rhavy.maia@gmail.com <rhavy.maia@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-10-27 17:47:22 +0000

    - Ajuste no cadastro do usuário.

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/servicos/CadastraUsuarioAsyncTask.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/servicos/CadastraUsuarioAsyncTask.java
@@ -4,75 +4,69 @@ import org.apache.http.HttpResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import br.edu.ifpb.recdata.telas.TelaListaFuncionalidadesPersonalizada;
-import br.edu.ifpb.recdata.telas.TelaLogin;
-import br.edu.ifpb.recdata.util.Constantes;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
+import br.edu.ifpb.recdata.telas.TelaLogin;
+import br.edu.ifpb.recdata.util.Constantes;
 
-public class CadastraUsuarioAsyncTask extends AsyncTask<JSONObject, Void, HttpResponse> {
+public class CadastraUsuarioAsyncTask extends
+		AsyncTask<JSONObject, Void, HttpResponse> {
 
 	Activity activity;
-	
-	public CadastraUsuarioAsyncTask( Activity activity) {
-	
+
+	public CadastraUsuarioAsyncTask(Activity activity) {
+
 		this.activity = activity;
-	
+
 	}
-	
+
 	@Override
 	protected void onPreExecute() {
 		super.onPreExecute();
 	}
-	
+
 	@Override
 	protected HttpResponse doInBackground(JSONObject... jsonObjects) {
-	    // Enviar a requisição HTTP via GET.
-        HttpService  httpService = new HttpService();
-        HttpResponse response = httpService.sendJsonPostRequest(
-                       "/usuario/cadastrar", jsonObjects[0]);
-        return response;
-}
+		// Enviar a requisição HTTP via GET.
+		HttpResponse response = HttpService.sendJsonPostRequest(
+				"/usuario/cadastrar", jsonObjects[0]);
+		return response;
+	}
 
 	@Override
-	protected void onPostExecute(HttpResponse response) {		
-		
-        
+	protected void onPostExecute(HttpResponse response) {
+
 		int httpCode = response.getStatusLine().getStatusCode();
 
-        try {
-                // Conversão do response ( resposta HTTP) para String.
-                String json = HttpUtil.entityToString(response);
-                Log.i("ReCDATA", "Resquest - POST: " + json);
+		try {
+			// Conversão do response ( resposta HTTP) para String.
+			String json = HttpUtil.entityToString(response);
+			Log.i("ReCDATA", "Resquest - POST: " + json);
 
-                JSONObject jsonObject = new JSONObject(json);
+			JSONObject jsonObject = new JSONObject(json);
 
-                if (httpCode >200 &&  httpCode < 400) {
-                        
-                        Toast.makeText(activity.getApplicationContext(),
-                                        Constantes.USUARIO_CADASTRADO,
-                                        Toast.LENGTH_SHORT).show();
+			if (httpCode > 200 && httpCode < 400) {
 
-                        Intent intent = new Intent(activity, TelaLogin
-                        		.class);
-                        activity.startActivity(intent);
-                        activity.finish();
+				Toast.makeText(activity.getApplicationContext(),
+						Constantes.USUARIO_CADASTRADO, Toast.LENGTH_SHORT)
+						.show();
 
-                } else {
-                        Toast.makeText(activity.getApplicationContext(),
-                                        jsonObject.getString("mensagem ERRO Ao Cadastra! "), Toast.LENGTH_SHORT)
-                                        .show();
-                }
+				Intent intent = new Intent(activity, TelaLogin.class);
+				activity.startActivity(intent);
+				activity.finish();
 
-        } catch (JSONException e) {
-                Log.e("ReCDATA", "Error parsing data " + e.toString());
-        }
+			} else {
+				Toast.makeText(activity.getApplicationContext(),
+						jsonObject.getString("mensagem ERRO Ao Cadastra! "), //Confuso!
+						Toast.LENGTH_SHORT).show();
+			}
 
-
+		} catch (JSONException e) {
+			Log.e("ReCDATA", "Error parsing data " + e.toString());
+		}
 
 	}
 }
--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaCadastraUsuario.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaCadastraUsuario.java
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -19,35 +18,46 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import br.edu.ifpb.R;
 import br.edu.ifpb.recdata.servicos.CadastraUsuarioAsyncTask;
-import br.edu.ifpb.recdata.servicos.UsuarioLoginAsyncTask;
 
 public class TelaCadastraUsuario extends Activity implements OnClickListener {
 
 	private List<String> itens_user = new ArrayList<String>();
 
 	// atributos para montar o usuario e envia para o servidor
-	EditText nome;
-	EditText cpf;
-	EditText login;
-	EditText senha;
-	DatePicker dataNascimento;
-	EditText endereco;
-	EditText telefone;
-	EditText email;
-	RadioGroup sexoRadio;
-	Spinner tipoUsuario;
-
-	/*
-	 * login_usuario,senha_usuario,nome_usuario,email_usuario,telefone_usuario,
-	 * cpf_usuario," +
-	 * "endereco_usuario,data_nasc_usuario,sexo_usuario,tb_tipousuario_idTipousuario
-	 */
+	private EditText nome;
+	private EditText cpf;
+	private EditText login;
+	private EditText senha;
+	private DatePicker dataNascimento;
+	private EditText endereco;
+	private EditText telefone;
+	private EditText email;
+	private RadioGroup sexoRadio;
+	private Spinner tipoUsuario;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_formulario_cadastrousuario);
 
+		carregarTiposUsuarios();
+		
+		Button Cadastrar = (Button) findViewById(R.id.criar_conta);
+		Cadastrar.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		
+		getViews();
+		JSONObject jsonObject = montarObjetoJSON();
+		CadastraUsuarioAsyncTask cadastrar = new CadastraUsuarioAsyncTask(this);
+		cadastrar.execute(jsonObject);
+	}
+
+	private void carregarTiposUsuarios() {
+		
 		// Opções de usuario
 		itens_user.add("Selecione...");
 		itens_user.add("Professor");
@@ -55,52 +65,17 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 		itens_user.add("Monitor");
 
 		tipoUsuario = (Spinner) findViewById(R.id.tipousuario);
-        
+
 		this.ativarSpinner(tipoUsuario, itens_user);
-
-		Button Cadastrar = (Button) findViewById(R.id.criar_conta);
-		Cadastrar.setOnClickListener(this);
-
 	}
 
-/*	public String capturavalorTipoUsuario(){
-	
-		 final String descricaoUsuario;
-		
-		tipoUsuario.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			@Override
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int position, long id) {
-			
-				descricaoUsuario = (String) parent.getItemAtPosition(position);
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
-
-			
-		});	
-		return descricaoUsuario;
-	}
-	*/
 	public void ativarSpinner(Spinner generico, List<String> itens_genericos) {
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_spinner_item, itens_genericos);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 		generico.setAdapter(adapter);
 		generico.setSelection(0);
-
-	
 	}
-
-	/*
-	 * "usuarioId":2, "nomeUsuario":"wemerson Thayne",
-	 * "emailUsuario":"wemerson@thayne.com", "telefoneUsuario":"11278944",
-	 * "idadeUsuario":"1986-08-10", "sexoUsuario":"M", "cpfUsuario":"491456200",
-	 * "enderecoUsuario":"Rua dos desenvolvedores", "loginUsuario":"wemersont",
-	 * "senhaUsuario":null, "idTipoUsuario":2, "descricao_tipoUsuario":null
-	 */
 
 	public String capturavalor() {
 
@@ -115,7 +90,6 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 		case R.id.sexoFeminino:
 			sexoEscolhido = "F";
 			break;
-
 		default:
 			break;
 		}
@@ -133,44 +107,37 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 				+ String.valueOf(dataNascMes) + "-"
 				+ String.valueOf(dataNascDia);
 
-		// ==========================================
-
 		return dataNascimentoCompleto;
 	}
 
 	public JSONObject montarObjetoJSON() {
 
-		JSONObject jsonObject = null;
+		JSONObject jsonObject = new JSONObject();
 
 		try {
-			jsonObject.put("nomeUsuario",nome.getText().toString() );
+			jsonObject.put("nomeUsuario", nome.getText().toString());
 			jsonObject.put("emailUsuario", email.getText().toString());
-			jsonObject.put("telefoneUsuario",telefone.getText().toString());
-			jsonObject.put("idadeUsuario",montarHoraPicker());
-			jsonObject.put("sexoUsuario",capturavalor());
+			jsonObject.put("telefoneUsuario", telefone.getText().toString());
+			jsonObject.put("idadeUsuario", montarHoraPicker());
+			jsonObject.put("sexoUsuario", capturavalor());
 			jsonObject.put("cpfUsuario", cpf.getText().toString());
 			jsonObject.put("enderecoUsuario", endereco.getText().toString());
 			jsonObject.put("loginUsuario", login.getText().toString());
 			jsonObject.put("senhaUsuario", senha.getText().toString());
+			jsonObject.put("idTipoUsuario", 2);
 			
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		return jsonObject;
-
 	}
-
-	@Override
-	public void onClick(View v) {
-
-		JSONObject jsonObject = montarObjetoJSON();
-		 CadastraUsuarioAsyncTask  cadastrar= new CadastraUsuarioAsyncTask(this);
-		cadastrar.execute(jsonObject);
-
+	
+	private void getViews() {
+		//TODO Pegar todos os campos com o Id definido no layout.
+		this.nome = (EditText) findViewById(R.id.nome_completo);
+		//...
 	}
-
 }

b121725bb1809329d5eabb906acea66d926bf4bb
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-10-29 10:44:15 +0000

    -implementação de serviço de reserva

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Constantes.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Constantes.java
@@ -2,6 +2,15 @@ package br.edu.ifpb.recdata.util;
 
 public class Constantes {
 
+	//mensagens de Sucesso
 	public static String USUARIO_CADASTRADO = "USUÁRIO CADASTRADO COM SUCESSO!";
+	public static String RESERVA_CONCLUIDA = "RESERVA CONCLUIDO COM SUCESSO!";
+	public static String ITEM_ENCONTRADO = "ITEM ENCONTRADO!";
+	
+	// mensagens de erros
+	public static String USUARIO_NAO_CADASTRADO =  "NÃO FOI POSSIVEL CADASTRAR USUÁRIO!";
+	public static String RESERVA_NAO_CONCLUIDA = "NÃO FOI POSSIVEL FAZER A RESERVA!";
+	public static String ITEM_NAO_ENCONTRADO = "ITEM NÃO ENCONTRADO!";
+	
 	
 }

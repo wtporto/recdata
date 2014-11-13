07c880e133eaa2d8aa538a1052c242349a81561c
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-11-13 13:12:30 +0000

    alterações em todas as tabelas do banco

--- trunk/BancoDados/RecData-BD-Versão-4.1.sql
+++ trunk/BancoDados/RecData-BD-Versão-4.1.sql
@@ -1,104 +1,140 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `recdata` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 USE `recdata` ;
+--
+-- Database: recdata
+--
+-- --------------------------------------------------------
+--
+-- Estrutura da tabela tb_regiao
+--
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_categoria` (
-  `IdCategoria` INT NOT NULL AUTO_INCREMENT,
-  `descricao_categoria` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`IdCategoria`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS tb_regiao (
+  cd_regiao int(11) NOT NULL AUTO_INCREMENT,
+  nm_regiao varchar(60) NOT NULL,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_regiao)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_item` (
-  `idItem` INT NOT NULL AUTO_INCREMENT,
-  `tb_categoria_IdCategoria` INT NOT NULL,
-  `descricao_item` VARCHAR(60) NOT NULL,
-  PRIMARY KEY (`idItem`),
-  INDEX `fk_Item_tb_categoria1_idx` (`tb_categoria_IdCategoria` ASC),
-  CONSTRAINT `fk_Item_tb_categoria1`
-    FOREIGN KEY (`tb_categoria_IdCategoria`)
-    REFERENCES `recdata`.`tb_categoria` (`IdCategoria`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Estrutura da tabela tb_categoria
+--
 
+CREATE TABLE IF NOT EXISTS tb_categoria (
+  cd_categoria int(11) NOT NULL AUTO_INCREMENT,
+  nm_descricao varchar(45) NOT NULL,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_categoria)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_tipousuario` (
-  `idTipousuario` INT NOT NULL AUTO_INCREMENT,
-  `descricao_tipousuario` VARCHAR(40) NOT NULL,
-  PRIMARY KEY (`idTipousuario`))
-ENGINE = InnoDB;
+-- --------------------------------------------------------
+--
+-- Estrutura da tabela tb_item
+--
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_usuario` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT,
-  `login_usuario` VARCHAR(40) NOT NULL UNIQUE,
-  `senha_usuario` VARCHAR(23) NOT NULL,
-  `nome_usuario` VARCHAR(50) NOT NULL,
-  `email_usuario` VARCHAR(45) NOT NULL UNIQUE,
-  `telefone_usuario` VARCHAR(10) NOT NULL,
-  `cpf_usuario` VARCHAR(11) NOT NULL,
-  `endereco_usuario` VARCHAR(70) NULL,
-  `data_nasc_usuario` DATE NOT NULL,
-  `sexo_usuario` VARCHAR(1) NOT NULL,
-  `tb_tipousuario_idTipousuario` INT NOT NULL,
-  PRIMARY KEY (`idUsuario`),
-  INDEX `fk_tb_usuario_tb_tipousuario1_idx` (`tb_tipousuario_idTipousuario` ASC),
-  CONSTRAINT `fk_tb_usuario_tb_tipousuario1`
-    FOREIGN KEY (`tb_tipousuario_idTipousuario`)
-    REFERENCES `recdata`.`tb_tipousuario` (`idTipousuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS tb_item (
+  cd_item int(11) NOT NULL AUTO_INCREMENT,
+  cd_categoria int(11) NOT NULL,
+  cd_regiao int(11) NOT NULL,
+  nm_item varchar(60) NOT NULL, -- Antiga descrição;
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_item),
+  KEY fk_tb_categoria_idx (cd_categoria),
+  KEY fk_tb_regiao_idx (cd_regiao)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_reserva` (
-  `idReserva` INT NOT NULL AUTO_INCREMENT,
-  `tb_item_idItem` INT NOT NULL,
-  `tb_usuario_idUsuario` INT NOT NULL,
-  `data_inicio` DATE NOT NULL,
-  `hora_inicio` TIME NOT NULL,
-  `data_fim` DATE NOT NULL,
-  `hora_fim` TIME NOT NULL,
-  PRIMARY KEY (`idReserva`),
-  INDEX `fk_tb_reserva_tb_item1_idx` (`tb_item_idItem` ASC),
-  INDEX `fk_tb_reserva_tb_usuario1_idx` (`tb_usuario_idUsuario` ASC),
-  CONSTRAINT `fk_tb_reserva_tb_item1`
-    FOREIGN KEY (`tb_item_idItem`)
-    REFERENCES `recdata`.`tb_item` (`idItem`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tb_reserva_tb_usuario1`
-    FOREIGN KEY (`tb_usuario_idUsuario`)
-    REFERENCES `recdata`.`tb_usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Estrutura da tabela tb_reserva
+--
 
-CREATE TABLE IF NOT EXISTS `recdata`.`tb_devolucao` (
-  `idDevolucao` INT NOT NULL AUTO_INCREMENT,
-  `tb_item_idItem` INT NOT NULL,
-  `tb_usuario_idUsuario` INT NOT NULL,
-  `data_devolucao` DATE NOT NULL,
-  `hora_devolucao` TIME NOT NULL,
-  PRIMARY KEY (`idDevolucao`),
-  INDEX `fk_tb_devolucao_tb_item1_idx` (`tb_item_idItem` ASC),
-  INDEX `fk_tb_devolucao_tb_usuario1_idx` (`tb_usuario_idUsuario` ASC),
-  CONSTRAINT `fk_tb_devolucao_tb_item1`
-    FOREIGN KEY (`tb_item_idItem`)
-    REFERENCES `recdata`.`tb_item` (`idItem`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tb_devolucao_tb_usuario1`
-    FOREIGN KEY (`tb_usuario_idUsuario`)
-    REFERENCES `recdata`.`tb_usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS tb_reserva (
+  cd_reserva int(11) NOT NULL AUTO_INCREMENT,
+  cd_item int(11) NOT NULL,
+  cd_usuario_reserva int(11) NOT NULL,
+  nm_observacao_reserva VARCHAR(255), 
+  data_inicio date NOT NULL,
+  hora_inicio time NOT NULL,
+  data_fim date NOT NULL,
+  hora_fim time NOT NULL,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (idReserva),
+  KEY fk_tb_reserva_tb_item_idx (cd_item),
+  KEY fk_tb_reserva_tb_usuario_reserva_idx (cd_usuario_reserva)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Estrutura da tabela tb_retirada
+--
+
+CREATE TABLE IF NOT EXISTS tb_retirada (
+  cd_retirada int(11) NOT NULL AUTO_INCREMENT,
+  cd_reserva int(11) NOT NULL,
+  cd_usuario_liberacao int(11) NOT NULL,
+  dt_retirada timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_retirada),
+  KEY fk_tb_retirada_tb_reserva_idx (cd_reserva),
+  KEY fk_tb_retirada_tb_usuario_liberacao_idx (cd_usuario_liberacao)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela tb_devolucao
+--
+
+CREATE TABLE IF NOT EXISTS tb_devolucao (
+  cd_devolucao int(11) NOT NULL AUTO_INCREMENT,
+  cd_reserva int(11) NOT NULL,
+  cd_usuario_recebimento int(11) NOT NULL, -- Usuário que recebe o item devolvido.
+  cd_usuario_devolucao int(11) NOT NULL, -- Usuário que devolve o item. 
+  dt_devolucao timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_devolucao),
+  KEY fk_tb_devolucao_tb_reserva_idx (cd_reserva),
+  KEY fk_tb_devolucao_tb_usuario_recebimento_idx (cd_usuario_recebimento),
+  KEY fk_tb_devolucao_tb_usuario_devolucao_idx (cd_usuario_devolucao)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela tb_tipousuario
+--
+
+CREATE TABLE IF NOT EXISTS tb_tipousuario (
+  cd_tipousuario int(11) NOT NULL AUTO_INCREMENT,
+  nm_tipousuario varchar(40) NOT NULL, -- Antigo descrição.
+  PRIMARY KEY (cd_tipousuario)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela tb_usuario
+--
+
+CREATE TABLE IF NOT EXISTS tb_usuario (
+  cd_usuario int(11) NOT NULL AUTO_INCREMENT,
+  nm_login varchar(40) NOT NULL,
+  nm_senha varchar(23) NOT NULL,
+  nm_usuario varchar(50) NOT NULL,
+  nm_email varchar(45) NOT NULL,
+  nr_telefone varchar(10) NOT NULL,
+  nr_cpf varchar(11) NOT NULL,
+  nm_endereco varchar(70) DEFAULT NULL,
+  dt_nascimento date NOT NULL,
+  tp_sexo varchar(1) NOT NULL,
+  cd_tipousuario int(11) NOT NULL,
+  dt_registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (cd_usuario),
+  UNIQUE KEY nm_login (nm_login),
+  UNIQUE KEY nm_email (nm_email),
+  KEY fk_tb_usuario_tb_tipousuario_idx (cd_tipousuario)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;

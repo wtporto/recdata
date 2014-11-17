6c0d3133b4fc23277296757f3722b935ee2d2d5d
rhavy.maia@gmail.com <rhavy.maia@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-11-17 16:47:04 +0000

    - Refactoring da Categoria

--- trunk/trunk/RecCDATA/src/br/edu/ifpb/recdata/entidades/Categoria.java
+++ trunk/trunk/RecCDATA/src/br/edu/ifpb/recdata/entidades/Categoria.java
@@ -1,49 +1,53 @@
 package br.edu.ifpb.recdata.entidades;
 
+import java.util.Date;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Categoria {
 
-	private int idCategoria;
-	private String descricaoCategoria;
+	private int id;
+	
+	private String descricao;
+	
+	private Date registro;
 	
 	public Categoria(int id) {
-		setIdCategoria(id);
+		setId(id);
 	}
-	public Categoria(){
-		
-	}
+	
+	public Categoria(){}
 
-	public Categoria(int id, String categoria){
-		
-		setIdCategoria(id);
-		setDescricaoCategoria(categoria);
+	public Categoria(int id, String categoria){		
+		setId(id);
+		setDescricao(categoria);
 	}
 	
 	@XmlElement
-	public int getIdCategoria() {
-		return idCategoria;
+	public int getId() {
+		return id;
 	}
 
-	public void setIdCategoria(int idCategoria) {
-		this.idCategoria = idCategoria;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	@XmlElement
-	public String getDescricaoCategoria() {
-		return descricaoCategoria;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setDescricaoCategoria(String descricaoCategoria) {
-		this.descricaoCategoria = descricaoCategoria;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	@Override
 	public String toString() {
-		return "Categoria [idCategoria=" + idCategoria
-				+ ", descricaoCategoria=" + descricaoCategoria + "]";
+		return "Categoria [idCategoria=" + id
+				+ ", descricaoCategoria=" + descricao 
+				+ "registro=" + registro + "]";
 	}
 	
 }

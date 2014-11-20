534d05721c995f4e22fbd71ff366080dd992c52f
rhavy.maia@gmail.com <rhavy.maia@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-11-20 03:18:36 +0000

    - Consultar item para reserva.

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/servicos/BuscaItensServidorAsyncTask.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/servicos/BuscaItensServidorAsyncTask.java
@@ -1,9 +1,8 @@
 package br.edu.ifpb.recdata.servicos;
 
-import java.io.Serializable;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,7 +12,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import br.edu.ifpb.recdata.telas.TelaConsultarItem;
 import br.edu.ifpb.recdata.telas.TelaResultadoItem;
 import br.edu.ifpb.recdata.util.Constantes;
 
@@ -46,29 +44,40 @@ public class BuscaItensServidorAsyncTask extends
 
 		int httpCode = response.getStatusLine().getStatusCode();
 
-		if (httpCode >= 200 && httpCode < 400) {
+		if (httpCode == HttpStatus.SC_OK) {
 
 			// Conversão do response ( resposta HTTP) para String.
 			String itensJson = HttpUtil.entityToString(response);
 			Log.i("ReCDATA ", "Resquest - POST: " + itensJson);
 
-			if (httpCode == HttpStatus.SC_ACCEPTED) {
+			// TODO: Deve transforma em JsonArray para verificar quantos itens foram
+			// encontrados, caso maior que um redirecinar a intent enviando a
+			// String do Json
 
-				Toast.makeText(activity.getApplicationContext(),
-						Constantes.ITEM_ENCONTRADO, Toast.LENGTH_SHORT).show();
-				Intent intent = new Intent(this.activity, TelaResultadoItem.class);
-				Bundle bundle = new Bundle();
-				bundle.putString("itens", itensJson);
-				intent.putExtras(bundle);
-				this.activity.startActivity(intent);
+			try {
+				JSONArray jsonArray = new JSONArray(itensJson);
 
-			} else {
-				Toast.makeText(activity.getApplicationContext(),
-						Constantes.ITEM_NAO_ENCONTRADO, Toast.LENGTH_SHORT)
-						.show();
+				if (jsonArray.length() > 0) {
+					Toast.makeText(activity.getApplicationContext(),
+							Constantes.ITEM_ENCONTRADO, Toast.LENGTH_SHORT).show();
+					/*
+					 * Intent intent = new Intent(this.activity,
+					 * TelaResultadoItem.class); Bundle bundle = new Bundle();
+					 * bundle.putString("itens", itensJson);
+					 * intent.putExtras(bundle);
+					 * this.activity.startActivity(intent);
+					 */
+				} else {
+					Toast.makeText(activity.getApplicationContext(),
+							Constantes.ITEM_NAO_ENCONTRADO, Toast.LENGTH_SHORT)
+							.show();
+				}
+
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
 		}
-
 	}
 }

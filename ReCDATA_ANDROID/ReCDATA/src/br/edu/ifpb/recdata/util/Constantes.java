e0d8f333cbfc5c17df3b1895727990e23b7c3895
wemerson.thayne <wemerson.thayne@gmail.com>
2014-11-26 00:10:03 +0000

    implementação dos Serviços  buscaQr  buscaCategoria, Região e descrição do Item  Reserva  e Lista Reservas

--- ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Constantes.java
+++ ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/util/Constantes.java
@@ -6,14 +6,20 @@ public class Constantes {
 	public static String USUARIO_CADASTRADO = "USUÁRIO CADASTRADO COM SUCESSO!";
 	public static String RESERVA_CONCLUIDA = "RESERVA CONCLUIDO COM SUCESSO!";
 	public static String ITEM_ENCONTRADO = "ITEM ENCONTRADO!";
+	public static String RESERVA_ENCONTRADA = "RESERVA ENCONTRADO!";
+	public static String EXISTE_CONEXAO = "CONEXÃO ESTABELECIDA COM SUCESSO!";
 	
 	// mensagens de erros
 	public static String USUARIO_NAO_CADASTRADO =  "NÃO FOI POSSIVEL CADASTRAR USUÁRIO!";
 	public static String RESERVA_NAO_CONCLUIDA = "NÃO FOI POSSIVEL FAZER A RESERVA!";
 	public static String ITEM_NAO_ENCONTRADO = "ITEM NÃO ENCONTRADO!";
+	public static String RESERVA_NAO_ENCONTRADA = "RESERVA NÃO ENCONTRADO!";
+	public static String NA0_EXISTE_CONEXAO = "NÃO FOI POSSÍVEL ESTABELECER UMA CONEXÃO, VERIFIQUE SUA REDE!";
 	
 	// Campo não preenchidos
 	
 	private static String ErroPreenche= "CAMPO NÃO PREENCHIDO";
+	
+	
 	
 }

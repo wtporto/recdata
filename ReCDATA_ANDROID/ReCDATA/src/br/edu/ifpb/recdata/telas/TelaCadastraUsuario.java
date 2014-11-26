836d0693330405375103abb86596db0ca665af92
wemerson.thayne <wemerson.thayne@gmail.com>
2014-11-26 00:09:14 +0000

    implementação dos Serviços  buscaQr  buscaCategoria, Região e descrição do Item  Reserva  e Lista Reservas

--- ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaCadastraUsuario.java
+++ ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaCadastraUsuario.java
@@ -10,12 +10,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 import br.edu.ifpb.R;
 import br.edu.ifpb.recdata.servicos.CadastraUsuarioAsyncTask;
 
@@ -34,6 +36,7 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 	private EditText email;
 	private RadioGroup sexoRadio;
 	private Spinner tipoUsuario;
+	private int idtipoUsuario;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,30 +45,29 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_formulario_cadastrousuario);
 
 		carregarTiposUsuarios();
-		
+		getViews();
+
 		Button Cadastrar = (Button) findViewById(R.id.criar_conta);
 		Cadastrar.setOnClickListener(this);
 	}
-	
+
 	@Override
 	public void onClick(View v) {
-		
-		getViews();
+
 		JSONObject jsonObject = montarObjetoJSON();
 		CadastraUsuarioAsyncTask cadastrar = new CadastraUsuarioAsyncTask(this);
 		cadastrar.execute(jsonObject);
 	}
 
 	private void carregarTiposUsuarios() {
-		
+
 		// Opções de usuario
 		itens_user.add("Selecione...");
 		itens_user.add("Professor");
-		itens_user.add("Servidor");
+		itens_user.add("Gestor de Turno");
 		itens_user.add("Monitor");
 
 		tipoUsuario = (Spinner) findViewById(R.id.tipousuario);
-
 		this.ativarSpinner(tipoUsuario, itens_user);
 	}
 
@@ -75,9 +77,36 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 		generico.setAdapter(adapter);
 		generico.setSelection(0);
+
+		generico.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				if (arg2 == 0) {
+					Toast.makeText(
+							TelaCadastraUsuario.this.getApplicationContext(),
+							"Item Escolhido , Invalido!", Toast.LENGTH_SHORT)
+							.show();
+				}
+				
+				if (arg2 == 1) {
+					idtipoUsuario = 1;
+				}
+				if (arg2 == 2) {
+					idtipoUsuario = 2;
+				}
+				if (arg2 == 3) {
+					idtipoUsuario = 3;
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {}
+		});
 	}
 
-	public String capturavalor() {
+	public String capturavalorRadioGroup() {
 
 		String sexoEscolhido = null;
 
@@ -97,11 +126,11 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 		return sexoEscolhido;
 	}
 
-	public String montarHoraPicker() {
+	public String montarHoraPicker(DatePicker datepicker) {
 		// montar a data
-		int dataNascAno = dataNascimento.getYear();
-		int dataNascMes = dataNascimento.getMonth();
-		int dataNascDia = dataNascimento.getDayOfMonth();
+		int dataNascAno = datepicker.getYear();
+		int dataNascMes = datepicker.getMonth();
+		int dataNascDia = datepicker.getDayOfMonth();
 
 		String dataNascimentoCompleto = String.valueOf(dataNascAno) + "-"
 				+ String.valueOf(dataNascMes) + "-"
@@ -112,19 +141,23 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 
 	public JSONObject montarObjetoJSON() {
 
-		JSONObject jsonObject = new JSONObject();
+		JSONObject usuarioJsonObject = new JSONObject();
 
 		try {
-			jsonObject.put("nomeUsuario", nome.getText().toString());
-			jsonObject.put("emailUsuario", email.getText().toString());
-			jsonObject.put("telefoneUsuario", telefone.getText().toString());
-			jsonObject.put("idadeUsuario", montarHoraPicker());
-			jsonObject.put("sexoUsuario", capturavalor());
-			jsonObject.put("cpfUsuario", cpf.getText().toString());
-			jsonObject.put("enderecoUsuario", endereco.getText().toString());
-			jsonObject.put("loginUsuario", login.getText().toString());
-			jsonObject.put("senhaUsuario", senha.getText().toString());
-			jsonObject.put("idTipoUsuario", 2);
+			usuarioJsonObject.put("nome", nome.getText().toString());
+			usuarioJsonObject.put("email", email.getText().toString());
+			usuarioJsonObject.put("telefone", telefone.getText().toString());
+			usuarioJsonObject.put("nascimento", montarHoraPicker(dataNascimento));
+			usuarioJsonObject.put("sexo", capturavalorRadioGroup());
+			usuarioJsonObject.put("cpf", cpf.getText().toString());
+			usuarioJsonObject.put("endereco", endereco.getText().toString());
+			usuarioJsonObject.put("login", login.getText().toString());
+			usuarioJsonObject.put("senha", senha.getText().toString());
+
+			JSONObject tipoUsuarioJsonObject = new JSONObject();
+			tipoUsuarioJsonObject.put("id", idtipoUsuario);
+			
+			usuarioJsonObject.put("tipoUsuario", tipoUsuarioJsonObject);
 			
 
 		} catch (JSONException e) {
@@ -132,12 +165,22 @@ public class TelaCadastraUsuario extends Activity implements OnClickListener {
 			e.printStackTrace();
 		}
 
-		return jsonObject;
+		return usuarioJsonObject;
 	}
-	
+
 	private void getViews() {
-		//TODO Pegar todos os campos com o Id definido no layout.
+		// TODO Pegar todos os campos com o Id definido no layout.
 		this.nome = (EditText) findViewById(R.id.nome_completo);
-		//...
+		this.cpf = (EditText) findViewById(R.id.cpf);
+		this.login = (EditText) findViewById(R.id.login);
+		this.senha = (EditText) findViewById(R.id.senha);
+		this.dataNascimento = (DatePicker) findViewById(R.id.dateNascimento);
+		this.dataNascimento.init(1995, 11, 21, null);
+		this.endereco = (EditText) findViewById(R.id.endereco);
+		this.telefone = (EditText) findViewById(R.id.telefone);
+		this.email = (EditText) findViewById(R.id.email);
+		;
+		this.sexoRadio = (RadioGroup) findViewById(R.id.sexo);
+		this.tipoUsuario = (Spinner) findViewById(R.id.tipousuario);
 	}
 }

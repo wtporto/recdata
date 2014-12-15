0504f460cd97a25d7bca83c46d6265c313482b64
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2014-12-15 14:36:45 +0000

    -gif de carregamento

--- trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaAbertura.java
+++ trunk/ReCDATA_ANDROID/ReCDATA/src/br/edu/ifpb/recdata/telas/TelaAbertura.java
@@ -2,26 +2,39 @@ package br.edu.ifpb.recdata.telas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import br.edu.ifpb.R;
 import br.edu.ifpb.recdata.servicos.VerificaServidorOnlineAsyncTasck;
 
-public class TelaAbertura extends Activity {
+public class TelaAbertura extends Activity implements Runnable{
 
-	@Override
+	 private int duracao_da_tela = 3000; 
+	
+	 @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_abertura);
 		
-		VerificaServidorOnlineAsyncTasck verificaConexao = 
-				new VerificaServidorOnlineAsyncTasck(this);
-		verificaConexao.execute();
+		new Handler().postDelayed(this, duracao_da_tela);
+
 	}
 	
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
 		Log.i("ReCDATA", "Fechando a aplicação.");
 	}
+	
+	@Override
+	public void run() {
+		
+		VerificaServidorOnlineAsyncTasck verificaConexao = 
+				new VerificaServidorOnlineAsyncTasck(this);
+		verificaConexao.execute();
+		
+		
+	}
+    
 }
+

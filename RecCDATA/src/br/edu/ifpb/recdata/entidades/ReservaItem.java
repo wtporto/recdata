796f9dd5fd60b42916db5d595dbd05c35d8d94ef
rhavy.maia <rhavy.maia@gmail.com>
2015-01-27 19:12:42 +0000

    - Entidade que representa a retirada do item.

--- RecCDATA/src/br/edu/ifpb/recdata/entidades/ReservaItem.java
+++ RecCDATA/src/br/edu/ifpb/recdata/entidades/ReservaItem.java
@@ -2,27 +2,35 @@ package br.edu.ifpb.recdata.entidades;
 
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class ReservaItem implements Entidade {
 
 	private int id;
-	//TODO: private Usuario usuarioReserva; -- Usuário responsável por realizar a reserva.
-	private Usuario usuario;
+	
+	private Usuario controladorRecurso;
+
+	private Usuario usuarioReserva;
+	
 	private Item item;
+	
 	private String observacao;
+	
 	private Date horaDataInicio;
+	
 	private Date horaDataFim;
+	
 	private Date dataRegistro;
 	
 	public ReservaItem() {}
 	
-	public ReservaItem(int id, Usuario usuario, Item item,String observacao,
+	public ReservaItem(int id, Usuario usuarioReserva, Item item,String observacao,
 			Date horaDataInicio, Date horaDataFim) {
 		super();
 		this.id = id;
-		this.usuario = usuario;
+		this.usuarioReserva = usuarioReserva;
 		this.item = item;
 		this.horaDataInicio = horaDataInicio;
 		this.horaDataFim = horaDataFim;
@@ -31,53 +39,16 @@ public class ReservaItem implements Entidade {
 
 	@Override
 	public String toString() {
-		return "ReservaItem [idReserva=" + id + ", usuario="
-				+ usuario + ", item=" + item +" observacao="+observacao
-				+ ", horaDataInicio=" + horaDataInicio + ", horaDataFim="
-				+ horaDataFim + "]";
+		return "ReservaItem [idReserva=" + id 
+				+ ", controladorRecurso" + controladorRecurso 
+				+ ", usuarioResponsavel=" + usuarioReserva 
+				+ ", item=" + item 
+				+ " observacao=" + observacao
+				+ ", horaDataInicio=" + horaDataInicio 
+				+ ", horaDataFim=" + horaDataFim + "]";
 	}
 
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public Item getItem() {
-		return item;
-	}
-
-	public void setItem(Item item) {
-		this.item = item;
-	}
-
-	public Date getHoraDataInicio() {
-		return horaDataInicio;
-	}
-
-	public void setHoraDataInicio(Date horaDataInicio) {
-		this.horaDataInicio = horaDataInicio;
-	}
-
-	public Date getHoraDataFim() {
-		return horaDataFim;
-	}
-
-	public void setHoraDataFim(Date horaDataFim) {
-		this.horaDataFim = horaDataFim;
-	}
-
-	public Date getDataRegistro() {
-		return dataRegistro;
-	}
-
-	public void setDataRegistro(Date dataRegistro) {
-		this.dataRegistro = dataRegistro;
-	}
-
+	@XmlElement
 	public int getId() {
 		return id;
 	}
@@ -86,11 +57,66 @@ public class ReservaItem implements Entidade {
 		this.id = id;
 	}
 
+	@XmlElement
+	public Usuario getControladorRecurso() {
+		return controladorRecurso;
+	}
+
+	public void setControladorRecurso(Usuario controladorRecurso) {
+		this.controladorRecurso = controladorRecurso;
+	}
+
+	@XmlElement
+	public Usuario getUsuarioReserva() {
+		return usuarioReserva;
+	}
+
+	public void setUsuarioReserva(Usuario usuarioReserva) {
+		this.usuarioReserva = usuarioReserva;
+	}
+
+	@XmlElement
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	@XmlElement
 	public String getObservacao() {
 		return observacao;
 	}
 
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
-	}	
+	}
+
+	@XmlElement
+	public Date getHoraDataInicio() {
+		return horaDataInicio;
+	}
+
+	public void setHoraDataInicio(Date horaDataInicio) {
+		this.horaDataInicio = horaDataInicio;
+	}
+
+	@XmlElement
+	public Date getHoraDataFim() {
+		return horaDataFim;
+	}
+
+	public void setHoraDataFim(Date horaDataFim) {
+		this.horaDataFim = horaDataFim;
+	}
+
+	@XmlElement
+	public Date getDataRegistro() {
+		return dataRegistro;
+	}
+
+	public void setDataRegistro(Date dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
 }

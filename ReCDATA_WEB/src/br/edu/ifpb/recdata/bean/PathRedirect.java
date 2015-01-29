f822347ff58764b86efd648a6ff05a2ffded0601
rhavy.maia <rhavy.maia@gmail.com>
2015-01-29 14:12:18 +0000

    - Atualização de projeto para novo POM.

--- ReCDATA_WEB/src/br/edu/ifpb/recdata/bean/PathRedirect.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpb.recdata.bean;
-
-public class PathRedirect {
-	public static final String RESERVARITEM = "reservarItem.xhtml";
-	
-	public static final String LISTARITEM = "listarItem.xhtml";
-
-}

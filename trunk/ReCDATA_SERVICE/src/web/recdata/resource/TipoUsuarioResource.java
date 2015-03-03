1090a0f8296cc2c594750dc3ad2d9dcb1d2f09ea
wemerson.thayne@gmail.com <wemerson.thayne@gmail.com@d610198b-3107-92dc-ceaa-84c01cfe6f14>
2015-03-03 11:46:03 +0000

    -Criação do serviços do TipoUsuario

---
+++ trunk/ReCDATA_SERVICE/src/web/recdata/resource/TipoUsuarioResource.java
@@ -0,0 +1,23 @@
+package web.recdata.resource;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import web.recdata.controller.TipoUsarioController;
+import br.edu.ifpb.recdata.entidades.TipoUsuario;
+
+@Path("/tipousuario")
+public class TipoUsuarioResource {
+
+	@GET
+	@Path("/listar")
+	@Produces("application/json")
+	public List<TipoUsuario> listarTodos() throws SQLException {
+		List<TipoUsuario> categorias = new TipoUsarioController().listarTodos();
+		return categorias;
+	}
+}

174772ae4439a7bfc40ef26b075f7763cd28730e
wemerson.thayne <wemerson.thayne@gmail.com>
2015-04-01 00:12:50 +0000

    -alterações no serviço e no DAO

--- ReCDATA_SERVICE/src/web/recdata/resource/ReservaResource.java
+++ ReCDATA_SERVICE/src/web/recdata/resource/ReservaResource.java
@@ -2,6 +2,7 @@ package web.recdata.resource;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -10,13 +11,49 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import web.recdata.controller.ReservaController;
+import br.edu.ifpb.recdata.entidades.Item;
 import br.edu.ifpb.recdata.entidades.ReservaItem;
+import br.edu.ifpb.recdata.entidades.Usuario;
 
 @Path("/reserva")
 public class ReservaResource {
 
+	@GET
+	@Path("/entidade")
+	@Produces("application/json")
+	public Response entidade() throws SQLException {
+		ResponseBuilder builder = Response.status(Response.Status.OK);
+		builder.expires(new Date());
+		
+		Date data = new Date();
+		ReservaItem reserva = new ReservaItem();
+		reserva.setDataRegistro(data);
+		reserva.setHoraDataFim(data);
+		reserva.setHoraDataInicio(data);
+		reserva.setId(1);
+		reserva.setObservacao("Observação da reserva");
+		
+		Item item = new Item();
+		item.setId(2);
+		item.setDescricao("Descrição do item");
+		
+		reserva.setItem(item);
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(3);
+		usuario.setNome("Wemerson");
+		
+		reserva.setUsuarioAtendente(usuario);
+		reserva.setUsuarioReserva(usuario);
+		
+		builder.entity(reserva);
+		
+		return builder.build();
+	}
+	
 	@GET
 	@Path("/listar")
 	@Produces("application/json")
@@ -40,12 +77,15 @@ public class ReservaResource {
 		return new ReservaController().readById(reserva);
 	}
 
-	@POST
-	@Path("/criar")
-	@Consumes("application/json")
-	@Produces("application/json")
+	 @POST
+     @Path("/cadastrar")
+     @Consumes("application/json")
+     @Produces("application/json")
 	public Response creat(ReservaItem reserva) {
-		return new ReservaController().creat(reserva);
+		
+		Response response = new ReservaController().creat(reserva);
+		
+		return response;
 	}
 
 	@POST
